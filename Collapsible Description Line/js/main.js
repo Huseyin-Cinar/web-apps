dfb67d00daf39139b8a488fd8dc2c0dcbaad7559
Hüseyin Çınar <84516708+Huseyin-Cinar@users.noreply.github.com>
2023-02-28 10:59:31 +0300

    Add files via upload

---
+++ Collapsible Description Line/js/main.js
@@ -0,0 +1,20 @@
+﻿/*
+The Future
+https://futuree.netlify.app/kodlama/html-css-ve-js-ile-katlanabilir-aciklama-satiri-yapma
+*/
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
