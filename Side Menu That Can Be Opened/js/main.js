5d12020f2f0ab1d9a8cabbc0c760794d175742d3
Hüseyin Çınar <84516708+Huseyin-Cinar@users.noreply.github.com>
2023-02-28 10:56:34 +0300

    Add files via upload

---
+++ Side Menu That Can Be Opened/js/main.js
@@ -0,0 +1,12 @@
+﻿/*
+The Future
+https://futuree.netlify.app/kodlama/html-css-ve-js-ile-kapanip-acilabilen-yan-menu-yapma
+*/
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
